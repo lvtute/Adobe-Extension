76f6cce923b6d17bdce75c4833bac1e8d708a2ed
unknown <lvtute@gmail.com>
2020-07-26 23:06:28 +0700

    features tested successfully

--- com.example.ext/js/main.js
+++ com.example.ext/js/main.js
@@ -1,36 +1,63 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
 /*global $, window, location, CSInterface, SystemPath, themeManager*/
 
+/*Prevent your local data from losing.
+Don't edit if you don't get certain knowledge about this.
+Might crash your Adobe host app.
+*/
+(function(){
+	var isNS = (navigator.appName == "Netscape") ? 1 : 0;
+
+	if(navigator.appName == "Netscape") document.captureEvents(Event.MOUSEDOWN||Event.MOUSEUP);
+
+	function mischandler(){
+		return false;
+	}
+
+	function mousehandler(e){
+		var myevent = (isNS) ? e : event;
+		var eventbutton = (isNS) ? myevent.which : myevent.button;
+		if((eventbutton==2)||(eventbutton==3)) return false;
+	}
+	document.oncontextmenu = mischandler;
+	document.onmousedown = mousehandler;
+	document.onmouseup = mousehandler;
+})();
+
+
 // demo function to call jsx
 (function () {
-    'use strict';
+	var csInterface = new CSInterface();
 
-    var csInterface = new CSInterface();
-    
+	'use strict';
 
-    function init() {
-    	
-                
-        themeManager.init();
-                
-        $("#btn_test").click(function () {
-            csInterface.evalScript('sayHello()');
-        });
 
-    }
-        
-    init();
+
+	function init() {
+
+
+		themeManager.init();
+
+		$("#btn_test").click(function () {
+			csInterface.evalScript('sayHello()');
+		});
+
+		
+
+	}
+
+	init();
 
 }());
 
 
 // get path of extension
-(function () {
- 'use strict';
- var favsList = [];
-  var favsIndices = [];
- var path, slash;
- path = location.href;
+var getPathOfExtension =function () {
+	'use strict';
+	var favsList = [];
+	var favsIndices = [];
+	var path, slash;
+	path = location.href;
 	if(getOS() == "MAC") {
 		slash = "/";
 		path = path.substring(0, path.length - 11);
@@ -39,24 +66,51 @@
 		slash = "/";
 		path = path.substring(8, path.length - 11);
 	}
-console.log(path);
- }());
+	return path;
+};
 
 
+var getFiles = function (path){
+	var csInterface = new CSInterface();
+
+	csInterface.evalScript('getFilesFromPath("' + path + '")');
+	console.log("day la path: "+ path);
+}
+getFiles(getPathOfExtension()+"/resource");
 // get os
 function getOS() {
- 		var userAgent = window.navigator.userAgent,
- 		platform = window.navigator.platform,
- 		macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
- 		windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
- 		os = null;
- 		console.log(platform);
- 		if(macosPlatforms.indexOf(platform) != -1) {
- 			os = "MAC";
- 		} else if(windowsPlatforms.indexOf(platform) != -1) {
- 			os = "WIN";
- 		}
- 		return os;
+	var userAgent = window.navigator.userAgent,
+	platform = window.navigator.platform,
+	macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+	windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+	os = null;
+	console.log(platform);
+	if(macosPlatforms.indexOf(platform) != -1) {
+		os = "MAC";
+	} else if(windowsPlatforms.indexOf(platform) != -1) {
+		os = "WIN";
+	}
+	return os;
 
- 	}
-    
+}
+// get JSX json
+// var getJSX = function(path){
+// 	path += "/presets";
+// 	var csInterface = new CSInterface();
+// 	csInterface.evalScript('getJsxFiles("'+ path +'")', function(res){
+// 		var paths = JSON.parse(res);
+// 	} );
+// 	setTimeout(function(){
+// 		console.log(paths);
+// 	}, 2000);
+// }
+
+// test button
+
+$("#btn-test2").click(function(){
+			var csInterface = new CSInterface();
+			csInterface.evalScript('importAep("'+getPathOfExtension()+"/resource/AEP/Blur"+'")');
+			console.log("haha"+getPathOfExtension()+"/resource/AEP/Blur");
+		});
+
+
--- com.example.ext/jsx/hostscript.jsx
+++ com.example.ext/jsx/hostscript.jsx
@@ -1,7 +1,86 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
 /*global $, Folder*/
 
+// #include "json2.js"
 
 function sayHello(){
-    alert("hello from ExtendScript");
+	alert("hello from ExtendScript");
+}
+
+function getFilesFromPath(path){
+	var thisFolder = Folder(path);
+	var files = thisFolder.getFiles();
+	// alert(files);
+
+}
+
+function getLayerNames(arg) {
+	var layerNames = [];
+	var comp = app.project.activeItem;
+	for(var i = 1; i <= comp.numLayers; i++) {
+		layerNames.push(comp.layer(i).name);
+	}
+
+	return JSON.stringify(layerNames);
+}
+
+function osCheck() {
+	var os = $.os;
+	var match = os.indexOf("Windows");
+	if(match != (-1)) {
+		var userOS = "PC";
+	} else {
+		var userOS = "MAC";
+	}
+	return userOS;
+}
+
+
+// demo get JSX files
+// function getJsxFiles(path){
+// 	var folder = Folder(path);
+// 	var files = folder.getFiles();
+// 	var jsxFiles = [];
+
+// 	for (var i =0;  i<files.length ; i++) {
+// 		if (files[i].name.indexOf('.jsx')!=-1) {
+// 			jsxFiles.push(files[i].name);
+// 		}
+// 	}
+// 	return JSON.stringify(jsxFiles);
+// }
+
+// apply aep file
+function importAep(path){
+	// alert(path);
+	var folder = Folder(path);
+	var files = folder.getFiles();
+	var aepFiles = [];
+
+	for (var i = 0; i < files.length; i++) {
+		if (files[i].name.indexOf('.aep')!=-1) {
+			aepFiles.push(files[i].fsName);
+		}
+	}
+
+	// alert(aepFiles);
+	// alert(File(aepFiles[0]).exists);
+	
+	var item = app.project.importFile(new ImportOptions(File(aepFiles[0])));
+	var comp = app.project.item(1).items[2];
+	var comp2 = app.project.item(1).name;
+	// comp.openInViewer();
+	alert(comp.name);
+	alert("label: "+comp.label+"; "+"comment: "+ comp.comment +"; "+ comp.id);
+	alert(comp instanceof CompItem);
+	// comp.openInViewer();
+	var attr;
+	for( a in comp){
+		attr += a+"; ";
+	}
+	alert(attr);
+	
+	// app.project.items[1].remove();
+
+	
 }
